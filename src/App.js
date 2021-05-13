646a52be1404f3396b1b73ddc265a85c54e5643e
Gichure Maina <76261570+KyleGichez@users.noreply.github.com>
2021-05-13 16:04:00 +0300

    task tracker

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import {useState,useEffect} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+const App = ()=> {
+  const [showAddTask,SetShowAddTask]= useState (false)
+  const [tasks,setTasks] = useState([])
+
+  useEffect(()=>{
+    const getTasks = async ()=>{
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+  //Fetch Tasks
+  const fetchTasks= async ()=>{
+    const res= await fetch ('http://localhost:5000/tasks')
+    const data= await res.json()
+
+    return data
+  }
+  
+//Add Task
+const addTask=(task)=>{
+  const id = Math.floor(Math.random()*10000)+ 1
+  const newTask ={id, ...task}
+  setTasks([...tasks, newTask])
+}
+//Delete Task
+const deleteTask= async (id)=>{
+
+  await fetch(`http://localhost:5000/tasks/ $ {index}`,{
+    method:'DELETE',
+  })
+
+  setTasks(tasks.filter((task)=> task.id !== id))
+}
+//Toggle Reminder
+const toggleReminder=(id)=>{
+  setTasks(tasks.map((task)=>task.id === id ? {...task, reminder:!task.reminder}:task))
+}
+  
+  return (
+    <div className="container">
+     <Header  onAdd={()=>SetShowAddTask(!showAddTask)}showAdd={showAddTask}/>
+     {showAddTask && <AddTask onAdd={addTask}/>}
+     {tasks.length > 0 ?(<Tasks tasks={tasks} onDelete={deleteTask}
+     onToggle={toggleReminder}/>):('There are currently no tasks')}
+     
+     
+    </div>
+  );
+}
+
+export default App;
